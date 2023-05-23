93a521cf04a3ae121938a124873e91d597e1c1ea
Krishu7827 <117253066+Krishu7827@users.noreply.github.com>
2023-05-23 10:06:37 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ let {RoleRouter} = require("./Router/Role.router")
 let {ProductRouter} =  require("./Router/Product.Router")
 let app = express()
 
+app.get("/",(req,res)=>{
+    res.send("welcome to express app for using product >> /product, for Using user function like login,signup etc.>> /Role")
+})
 
 app.use("/product",ProductRouter)
 app.use("/Role",RoleRouter)
